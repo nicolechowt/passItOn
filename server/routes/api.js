068a310c5e30d9217dab018416ce9596ef5edbcf
farid64 <farid.sobhany@gmail.com>
2018-02-05 15:12:42 -0800

    database models and controllers

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,4 +5,20 @@ const api_controller = require('../controllers/api_controller');
 
 router.get('/', api_controller.index);
 
+router.get('/profile', api_controller.profile);
+router.post('profile', api_controller.saveProfile);
+
+router.post('/userInterest', api_controller.addInterest);
+
+router.post('/request', api_controller.request);
+
+router.post('/signup', api_controller.signup);
+
+router.get('/story', api_controller.loadStory);
+router.delete('/stroy', api_controller.deleteStory);
+
+router.get('/leaders', api_controller.getLeaders);
+
+
+
 module.exports = router;
